3db2bc84ed0f5b617d60d3443a7d8a7bea1f83eb
Marcin Tuleja <marcintuleja@gmail.com>
2018-05-24 22:07:56 +0200

    Refactor UserRepository according to User class refactor

--- src/main/java/com/codecool/queststore/repositories/UserRepository.java
+++ src/main/java/com/codecool/queststore/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.codecool.queststore.repositories;
 
 import com.codecool.queststore.model.entities.User;
+import com.codecool.queststore.specifications.RoleById;
 import com.codecool.queststore.specifications.SqlSpecification;
 
 import java.sql.SQLException;
@@ -22,7 +23,7 @@ public class UserRepository extends AbstractRepository<User> {
         super.preparedStatement.setString(2, entity.getFirstName());
         super.preparedStatement.setString(3, entity.getLastName());
         super.preparedStatement.setString(4, entity.getEmail());
-        super.preparedStatement.setInt(5, entity.getRoleId());
+        super.preparedStatement.setInt(5, entity.getRole().getId());
         super.preparedStatement.setInt(6, entity.getHashedPassword());
         super.preparedStatement.executeUpdate();
     }
@@ -34,7 +35,7 @@ public class UserRepository extends AbstractRepository<User> {
         super.preparedStatement.setString(2, entity.getFirstName());
         super.preparedStatement.setString(3, entity.getLastName());
         super.preparedStatement.setString(4, entity.getEmail());
-        super.preparedStatement.setInt(5, entity.getRoleId());
+        super.preparedStatement.setInt(5, entity.getRole().getId());
         super.preparedStatement.setInt(6, entity.getHashedPassword());
         super.preparedStatement.setString(7, entity.getLogin());
         super.preparedStatement.executeUpdate();
@@ -45,22 +46,30 @@ public class UserRepository extends AbstractRepository<User> {
         super.preparedStatement = super.dbConnection.prepareStatement(DELETE_QUERY);
         super.preparedStatement.setString(1, entity.getLogin());
         super.preparedStatement.executeUpdate();
-    }
+    } te
 
     @Override
-    List<User> deserializeEntities() throws SQLException {
+    List<User> deserializeEntities() throws PersistenceLayerException {
         List<User> users = new ArrayList<>();
 
-        while (super.resultSet.next()) {
-            String login = super.resultSet.getString("login");
-            String firstName = super.resultSet.getString("first_name");
-            String lastName = super.resultSet.getString("last_name");
-            String email = super.resultSet.getString("email");
-            int roleId = super.resultSet.getInt("role_id");
-            String hashedPassword = super.resultSet.getString("password");
+        try {
+            while (super.resultSet.next()) {
+                String login = super.resultSet.getString("login");
+                String firstName = super.resultSet.getString("first_name");
+                String lastName = super.resultSet.getString("last_name");
+                String email = super.resultSet.getString("email");
 
-            users.add(new User(login, firstName, lastName, email, hashedPassword, roleId));
+                Repository roleRepository = this.REPOSITORY_POOL.getRepository(Repositories.ROLE);
+                SqlSpecification getRoleById = new RoleById(super.resultSet.getInt("role_id"));
+                Role role = roleRepository.query(getRoleById);
+
+                String hashedPassword = super.resultSet.getString("password");
+
+                users.add(new User(login, firstName, lastName, email, role, hashedPassword));
+            }
+            return users;
+        } catch (SQLException e) {
+            throw new PersistenceLayerException("Can't get User from the database");
         }
-        return users;
     }
 }
