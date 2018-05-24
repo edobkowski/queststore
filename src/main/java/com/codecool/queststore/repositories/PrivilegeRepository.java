26ef8a3b4a2f67b43bd3590a4152066253019a52
edobkowski <edobkowski@gmail.com>
2018-05-24 21:17:18 +0200

    Implement PrivilegeRepository class

--- src/main/java/com/codecool/queststore/repositories/PrivilegeRepository.java
+++ src/main/java/com/codecool/queststore/repositories/PrivilegeRepository.java
@@ -5,27 +5,47 @@ import com.codecool.queststore.specifications.SqlSpecification;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.ArrayList;
 
 public class PrivilegeRepository extends AbstractRepository<Privilege> {
+    private static final String ADD_QUERY = "INSERT INTO artifacts(name) VALUES(?)";
+    private static final String EDIT_QUERY = "INSERT INTO artifacts(name) VALUES(?) WHERE id=?";
+    private static final String DELETE_QUERY = "DELETE * FROM artifacts WHERE id=?";
+
     public PrivilegeRepository() throws PersistenceLayerException {}
 
     @Override
     void addEntity(Privilege entity) throws SQLException {
-
+        super.preparedStatement = super.dbConnection.prepareStatement(ADD_QUERY);
+        super.preparedStatement.setString(1, entity.getName());
+        super.preparedStatement.executeUpdate();
     }
 
     @Override
     void updateEntity(Privilege entity) throws SQLException {
-
+        super.preparedStatement = super.dbConnection.prepareStatement(EDIT_QUERY);
+        super.preparedStatement.setString(1, entity.getName());
+        super.preparedStatement.setInt(2, entity.getId());
+        super.preparedStatement.executeUpdate();
     }
 
     @Override
     void deleteEntity(Privilege entity) throws SQLException {
-
+        super.preparedStatement = super.dbConnection.prepareStatement(DELETE_QUERY);
+        super.preparedStatement.setInt(1, entity.getId());
+        super.preparedStatement.executeUpdate();
     }
 
     @Override
     List<Privilege> deserializeEntities() throws SQLException {
-        return null;
+        List<Privilege> privileges = new ArrayList<>();
+
+        while (super.resultSet.next()) {
+            int id = super.resultSet.getInt("id");
+            String name = super.resultSet.getString("name");
+
+            privileges.add(new Privilege(id, name));
+        }
+        return privileges;
     }
 }
