4bf52395a0060817607eddf470d1839e26464035
Marcin Tuleja <marcintuleja@gmail.com>
2018-05-24 14:46:29 +0200

    Refactor RepositoryPool to inner factory class with proper Repositories enum

--- src/main/java/com/codecool/queststore/repositories/Repositories.java
+++ src/main/java/com/codecool/queststore/repositories/Repositories.java
@@ -1,24 +1,14 @@
 package com.codecool.queststore.repositories;
 
 public enum Repositories {
-    USER(new UserRepository()),
-    ROLE(new RoleRepository()),
-    PRIVILEGE(new PrivilegeRepository()),
-    MENTOR(new MentorRepository()),
-    STUDENT(new StudentRepository()),
-    CLASS(new CodecoolClassRepository()),
-    QUEST(new QuestRepository()),
-    ARTIFACT(new ArtifactRepository()),
-    WALLET(new WalletRepository()),
-    LEVEL(new LevelRepository());
-
-    private Repository repository;
-
-    Repositories(Repository repository) {
-        this.repository = repository;
-    }
-
-    protected Repository getRepository() {
-        return this.repository;
-    }
+    USER,
+    ROLE,
+    PRIVILEGE,
+    MENTOR,
+    STUDENT,
+    CODECOOL_CLASS,
+    QUEST,
+    ARTIFACT,
+    WALLET,
+    LEVEL;
 }
--- src/main/java/com/codecool/queststore/repositories/RepositoryPool.java
+++ src/main/java/com/codecool/queststore/repositories/RepositoryPool.java
@@ -10,11 +10,40 @@ public class RepositoryPool {
         this.repositories = new HashMap<>();
     }
 
-    public Repository getRepository(Repositories repositoryType) {
+    public Repository getRepository(Repositories repositoryType) throws PersistenceLayerException {
         if (!this.repositories.containsKey(repositoryType)) {
-            this.repositories.put(repositoryType, repositoryType.getRepository());
+            this.repositories.put(repositoryType, RepositoryFactory.makeRepository(repositoryType));
         }
 
         return this.repositories.get(repositoryType);
+    }
+
+    private static class RepositoryFactory {
+        public static Repository makeRepository(Repositories repositoryType) throws PersistenceLayerException {
+            switch (repositoryType) {
+                case USER:
+                    return new UserRepository();
+                case ROLE:
+                    return new RoleRepository();
+                case PRIVILEGE:
+                    return new PrivilegeRepository();
+                case MENTOR:
+                    return new MentorRepository();
+                case STUDENT:
+                    return new StudentRepository();
+                case ARTIFACT:
+                    return new ArtifactRepository();
+                case QUEST:
+                    return new QuestRepository();
+                case WALLET:
+                    return new WalletRepository();
+                case LEVEL:
+                    return new LevelRepository();
+                case CODECOOL_CLASS:
+                    return new CodecoolClassRepository();
+                default:
+                    return null;
+            }
+        }
     }
 }
