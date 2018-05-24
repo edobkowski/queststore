6509e1da61a7d44d5260364cf18164b9cf95cb04
Marcin Tuleja <marcintuleja@gmail.com>
2018-05-24 14:59:23 +0200

    Implement DML methods for ArtifactRepository

--- src/main/java/com/codecool/queststore/repositories/ArtifactRepository.java
+++ src/main/java/com/codecool/queststore/repositories/ArtifactRepository.java
@@ -3,26 +3,44 @@ package com.codecool.queststore.repositories;
 import com.codecool.queststore.model.Artifact;
 import com.codecool.queststore.specifications.SqlSpecification;
 
+import java.sql.SQLException;
 import java.util.List;
 
-public class ArtifactRepository implements Repository<Artifact> {
-    @Override
-    public void add(Artifact entity) {
+public class ArtifactRepository extends AbstractRepository<Artifact> {
+    public ArtifactRepository() throws PersistenceLayerException {}
 
+    private static final String ADD_QUERY = "INSERT INTO artifacts(name, description, price) VALUES(?,?,?)";
+    private static final String EDIT_QUERY = "INSERT INTO artifacts(name, description, price) VALUES(?,?,?) WHERE id=?";
+    private static final String DELETE_QUERY = "DELETE * FROM artifacts WHERE id=?";
+
+    @Override
+    void addEntity(Artifact entity) throws SQLException {
+        super.preparedStatement.setString(1, entity.getName());
+        super.preparedStatement.setString(2, entity.getDescription());
+        super.preparedStatement.setInt(3, entity.getPrice());
+        super.preparedStatement = super.dbConnection.prepareStatement(ADD_QUERY);
+        super.preparedStatement.executeUpdate();
     }
 
     @Override
-    public void update(Artifact entity) {
-
+    void updateEntity(Artifact entity) throws SQLException {
+        super.preparedStatement.setString(1, entity.getName());
+        super.preparedStatement.setString(2, entity.getDescription());
+        super.preparedStatement.setInt(3, entity.getPrice());
+        super.preparedStatement.setInt(4, entity.getId);
+        super.preparedStatement = super.dbConnection.prepareStatement(EDIT_QUERY);
+        super.preparedStatement.executeUpdate();
     }
 
     @Override
-    public void delete(Artifact entity) {
-
+    void deleteEntity(Artifact entity) throws SQLException {
+        super.preparedStatement.setId(1, entity.getId());
+        super.preparedStatement = super.dbConnection.prepareStatement(DELETE_QUERY);
+        super.preparedStatement.executeUpdate();
     }
 
     @Override
-    public List<Artifact> query(SqlSpecification sqlSpecification) {
+    List<Artifact> queryEntities(SqlSpecification sqlSpecification) throws SQLException {
         return null;
     }
 }
