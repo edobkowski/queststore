3f21659aa78052385790ad57cb6e4a9d49996ec3
Marcin Tuleja <marcintuleja@gmail.com>
2018-05-25 07:51:39 +0200

    Add class for testing UserRepository

---
+++ src/test/java/com/codecool/queststore/repositories/UserRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.codecool.queststore.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void addEntity() {
+    }
+
+    @Test
+    void updateEntity() {
+    }
+
+    @Test
+    void deleteEntity() {
+    }
+
+    @Test
+    void deserializeEntities() {
+    }
+}
