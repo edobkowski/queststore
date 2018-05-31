4ddc09062d191d2f4ae1e910bac453b383034495
Marcin Tuleja <marcintuleja@gmail.com>
2018-05-31 13:23:42 +0200

    Fix calling CodecoolClassRepository

--- src/main/java/com/codecool/queststore/repositories/CodecoolClassRepository.java
+++ src/main/java/com/codecool/queststore/repositories/CodecoolClassRepository.java
@@ -1,13 +1,14 @@
 package com.codecool.queststore.repositories;
 
+import com.codecool.queststore.mappers.CodecoolClassMapper;
 import com.codecool.queststore.model.entities.CodecoolClass;
-import com.codecool.queststore.specifications.SqlSpecification;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class CodecoolClassRepository extends AbstractRepository<CodecoolClass> {
-    public CodecoolClassRepository() throws PersistenceLayerException {}
+    public CodecoolClassRepository() throws PersistenceLayerException {
+        super.mapper = new CodecoolClassMapper();
+    }
 
     @Override
     void addEntity(CodecoolClass entity) throws SQLException {
@@ -22,10 +23,5 @@ public class CodecoolClassRepository extends AbstractRepository<CodecoolClass> {
     @Override
     void deleteEntity(CodecoolClass entity) throws SQLException {
 
-    }
-
-    @Override
-    List<CodecoolClass> deserializeEntities() throws SQLException {
-        return null;
     }
 }
