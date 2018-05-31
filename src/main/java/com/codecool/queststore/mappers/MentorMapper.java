955d6036828618b227bcce5f6ebb38e879a928c4
Marcin Tuleja <marcintuleja@gmail.com>
2018-05-31 12:59:42 +0200

    Minor clean-up of Mappers, change getting RepositoryPool instance

--- src/main/java/com/codecool/queststore/mappers/MentorMapper.java
+++ src/main/java/com/codecool/queststore/mappers/MentorMapper.java
@@ -1,19 +1,21 @@
 package com.codecool.queststore.mappers;
 
-import com.codecool.queststore.model.entities.Mentor;
-import com.codecool.queststore.model.entities.CodecoolClass;
 import com.codecool.queststore.repositories.Repository;
-import com.codecool.queststore.criteria.SqlCriteria;
-import com.codecool.queststore.criteria.CodecoolClassByMentor;
 import com.codecool.queststore.repositories.Repositories;
 import com.codecool.queststore.repositories.RepositoryPool;
 
+import com.codecool.queststore.model.entities.Mentor;
+import com.codecool.queststore.model.entities.CodecoolClass;
+
+import com.codecool.queststore.criteria.SqlCriteria;
+import com.codecool.queststore.criteria.CodecoolClassByMentorLogin;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class MentorMapper implements Mapper {
-
-    public static final RepositoryPool REPOSITORY_POOL = new RepositoryPool();
+    private static final RepositoryPool REPOSITORY_POOL = RepositoryPool.getInstance();
 
     @Override
     public Mentor map(ResultSet resultSet) throws SQLException {
