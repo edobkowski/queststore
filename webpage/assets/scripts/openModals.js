ae2f33ebe646fdbf20b6be790cf18cc953f28bb4
Marcin Tuleja <marcintuleja@gmail.com>
2018-06-07 14:31:36 +0200

    Change paths to modals, add paths to newly coded modals

--- webpage/assets/scripts/openModals.js
+++ webpage/assets/scripts/openModals.js
@@ -3,16 +3,31 @@ function openModal(buttonId) {
 
   switch (buttonId) {
     case "open-add-mentor-modal":
-      modalFilePath = "../../add_mentor_modal.html";
+      modalFilePath = "assets/modals/add_mentor_modal.html";
       break;
     case "open-edit-mentor-modal":
-      modalFilePath = "../../edit_mentor_modal.html";
+      modalFilePath = "assets/modals/edit_mentor_modal.html";
       break;
     case "open-remove-mentor-modal":
-      modalFilePath = "../../remove_mentor_modal.html";
+      modalFilePath = "assets/modals/remove_mentor_modal.html";
       break;
     case "open-add-class-modal":
-      modalFilePath = "../../add_class_modal.html";
+      modalFilePath = "assets/modals/add_class_modal.html";
+      break;
+    case "open-edit-class-modal":
+      modalFilePath = "assets/modals/edit_class_modal.html";
+      break;
+    case "open-remove-class-modal":
+      modalFilePath = "assets/modals/remove_class_modal.html";
+      break;
+    case "open-add-level-modal":
+      modalFilePath = "assets/modals/add_level_modal.html";
+      break;
+    case "open-edit-level-modal":
+      modalFilePath = "assets/modals/edit_level_modal.html";
+      break;
+    case "open-remove-level-modal":
+      modalFilePath = "assets/modals/remove_level_modal.html";
       break;
   }
   
