12c1944e8d70ef620770992d36fdc751de07c6c3
Kubset <kuba1228334@gmail.com>
2018-06-19 13:49:54 +0200

    implement Level entity

--- src/main/java/com/codecool/queststore/entities/Level.java
+++ src/main/java/com/codecool/queststore/entities/Level.java
@@ -1,4 +1,27 @@
 package com.codecool.queststore.entities;
 
 public class Level {
+
+    private int id;
+    private String name;
+    private int treshold;
+
+    Level(int id, String name, int treshold) {
+       this.id = id;
+       this.name = name;
+       this.treshold = treshold;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTreshold() {
+        return treshold;
+    }
+
 }
