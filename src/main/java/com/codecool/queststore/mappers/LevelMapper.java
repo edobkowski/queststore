1fdd827effab167ad1e442665c58f3acc9f4531b
Marcin Tuleja <marcintuleja@gmail.com>
2018-06-20 07:53:12 +0200

    Fix minor bugs, add mapping to JSON methods

--- src/main/java/com/codecool/queststore/mappers/LevelMapper.java
+++ src/main/java/com/codecool/queststore/mappers/LevelMapper.java
@@ -5,14 +5,41 @@ import com.codecool.queststore.repositories.PersistenceLayerException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class LevelMapper implements Mapper<Level> {
     @Override
     public Level map(ResultSet resultSet) throws SQLException, PersistenceLayerException {
         int id = resultSet.getInt("id");
         String name = resultSet.getString("name");
-        int treshold = resultSet.getInt("treshold");
+        int threshold = resultSet.getInt("threshold");
 
-        return new Level(id, name, treshold);
+        return new Level(id, name, threshold);
+    }
+
+    public String mapToJson(Level level) {
+        return String.format("{\"id\": \"%s\", \"name\": \"%s\", \"threshold\": %d}",
+                level.getId(),
+                level.getName(),
+                level.getThreshold());
+    }
+
+    public String mapToJson(List<Level> levels) {
+        StringBuilder json = new StringBuilder();
+
+        json.append("{\"levels\": [");
+
+        int indexOfLastElement = levels.size() - 1;
+        for (Level level : levels) {
+            json.append(mapToJson(level));
+
+            if (levels.indexOf(level) != indexOfLastElement) {
+                json.append(",");
+            }
+        }
+
+        json.append("]}");
+
+        return json.toString();
     }
 }
