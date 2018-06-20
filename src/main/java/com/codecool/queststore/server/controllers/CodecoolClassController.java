ffe52961e3ad9b175df616170aeeccd526e6dd3c
Marcin Tuleja <marcintuleja@gmail.com>
2018-06-20 07:50:58 +0200

    Add not yet implemented mentor and Codecool class endpoints

---
+++ src/main/java/com/codecool/queststore/server/controllers/CodecoolClassController.java
@@ -0,0 +1,11 @@
+package com.codecool.queststore.server.controllers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class CodecoolClassController implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {}
+}
---
+++ src/main/java/com/codecool/queststore/server/controllers/MentorController.java
@@ -0,0 +1,13 @@
+package com.codecool.queststore.server.controllers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class MentorController implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
