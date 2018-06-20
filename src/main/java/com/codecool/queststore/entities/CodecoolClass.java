5e0f78fc4033f8740c484713f721a9b6772d2ed6
Marcin Tuleja <marcintuleja@gmail.com>
2018-06-20 10:18:50 +0200

    Add setter for id

--- src/main/java/com/codecool/queststore/entities/CodecoolClass.java
+++ src/main/java/com/codecool/queststore/entities/CodecoolClass.java
@@ -17,6 +17,10 @@ public class CodecoolClass {
         return this.id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return this.name;
     }
