451cd33d6fef9608e0b62b33e008b372c9fc97d9
Kubset <kuba1228334@gmail.com>
2018-06-20 10:44:51 +0200

    Add LevelById criteria

---
+++ src/main/java/com/codecool/queststore/criteria/LevelById.java
@@ -0,0 +1,14 @@
+package com.codecool.queststore.criteria;
+
+import com.codecool.queststore.repositories.PersistenceLayerException;
+
+public class LevelById extends GetById {
+    static {
+        QUERY = "SELECT * FROM levels WHERE id=?";
+    }
+
+    public LevelById(int id) throws PersistenceLayerException {
+        super(id);
+    }
+
+}
