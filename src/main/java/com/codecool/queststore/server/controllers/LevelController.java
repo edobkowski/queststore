6f130415c232af972d9431132b2d076de449b0a1
Marcin Tuleja <marcintuleja@gmail.com>
2018-06-20 13:47:37 +0200

    WIP levels endpoint

--- src/main/java/com/codecool/queststore/server/controllers/LevelController.java
+++ src/main/java/com/codecool/queststore/server/controllers/LevelController.java
@@ -9,6 +9,7 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
 import java.util.List;
 
 public class LevelController implements HttpHandler {
@@ -25,28 +26,81 @@ public class LevelController implements HttpHandler {
         String response = "";
         String requestMethod = exchange.getRequestMethod();
 
+        URI uri = exchange.getRequestURI();
+        boolean uriHasIdentifier = URIparser.hasIdentifier(uri);
+        int identifier = uriHasIdentifier ? URIparser.parseIdentifierToInt(uri) : -1;
+
         if (requestMethod.equals("GET")) {
             try {
-                List<Level> allLevels = levelManager.getAll();
-                response = levelMapper.mapToJson(allLevels);
-                exchange.getResponseHeaders().set("Content-Type", "application/json");
+                List<Level> levels = uriHasIdentifier ? levelManager.get(identifier) : levelManager.getAll();
+                response = levelMapper.mapToJson(levels);
+
+                System.out.println("Levels size: " + levels.size());
+                System.out.println(response);
+
+                setResponseHeader(exchange, MimeType.JSON);
+                exchange.sendResponseHeaders(200, response.length());
             } catch (ServiceLayerException e) {
                 response = e.getMessage();
-                exchange.getResponseHeaders().set("Content-Type", "text/plain");
+
+                setResponseHeader(exchange, MimeType.TEXT);
+                exchange.sendResponseHeaders(500, response.length());
             }
         } else if (requestMethod.equals("POST")) {
             try {
                 levelManager.create("Focus day monk", 666);
+
+                exchange.sendResponseHeaders(201, response.length());
             } catch (ServiceLayerException e) {
                 response = e.getMessage();
-                exchange.getResponseHeaders().set("Content-Type", "text/plain");
+
+                setResponseHeader(exchange, MimeType.TEXT);
+                exchange.sendResponseHeaders(500, response.length());
+            }
+        } else if (requestMethod.equals("PUT")) {
+            try {
+                if (uriHasIdentifier) {
+                    int id = 5;
+                    String name = "Focus Day Noob";
+                    int threshold = 999;
+
+                    levelManager.edit(id, name, threshold);
+
+                    exchange.sendResponseHeaders(201, response.length());
+                } else {
+                    throw new ServiceLayerException("Request invalid: no identifier");
+                }
+            } catch (ServiceLayerException e) {
+                response = e.getMessage();
+
+                setResponseHeader(exchange, MimeType.TEXT);
+                exchange.sendResponseHeaders(500, response.length());
+            }
+        } else if (requestMethod.equals("DELETE")) {
+            try {
+                if (uriHasIdentifier) {
+                    levelManager.remove(identifier);
+
+                    exchange.sendResponseHeaders(204, response.length());
+                } else {
+                    throw new ServiceLayerException("Request invalid: no identifier");
+                }
+            } catch (ServiceLayerException e) {
+                response = e.getMessage();
+
+                setResponseHeader(exchange, MimeType.TEXT);
+                exchange.sendResponseHeaders(500, response.length());
             }
         }
-
-        exchange.sendResponseHeaders(200, response.length());
 
         OutputStream responseBody = exchange.getResponseBody();
         responseBody.write(response.getBytes());
         responseBody.close();
+    }
+
+    private void setResponseHeader(HttpExchange exchange, MimeType type) {
+        String mimeTypeCode = type.getTypeCode();
+
+        exchange.getResponseHeaders().set("Content-Type", mimeTypeCode);
     }
 }
