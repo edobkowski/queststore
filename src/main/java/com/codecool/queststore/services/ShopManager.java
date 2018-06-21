357e298ad3886c6695a8449f33a5ff7a8f123632
Eryk <edobkowski@gmail.com>
2018-06-21 19:29:44 +0200

    Implement buyArtifact() method

--- src/main/java/com/codecool/queststore/services/ShopManager.java
+++ src/main/java/com/codecool/queststore/services/ShopManager.java
@@ -1,8 +1,11 @@
 package com.codecool.queststore.services;
 
 import com.codecool.queststore.criteria.SqlCriteria;
-import com.codecool.queststore.repositories.PersistenceLayerException;
-import com.codecool.queststore.repositories.RepositoryPool;
+import com.codecool.queststore.criteria.WalletArtifactsById;
+import com.codecool.queststore.criteria.WalletByOwnerLogin;
+import com.codecool.queststore.entities.Wallet;
+import com.codecool.queststore.entities.WalletArtifacts;
+import com.codecool.queststore.repositories.*;
 
 public class ShopManager {
     private static final RepositoryPool REPOSITORY_POOL;
@@ -11,11 +14,21 @@ public class ShopManager {
         REPOSITORY_POOL = RepositoryPool.getInstance();
     }
 
-    public void buyArtifact(int login, int artifactId) throws ServiceLayerException {
+    public void buyArtifact(String login, int artifactId) throws ServiceLayerException {
 
         try {
-            //TODO edit student-artifact table
-            throw new PersistenceLayerException("todo");
+            Repository<Wallet> walletRepository = (WalletRepository)REPOSITORY_POOL
+                    .getRepository(Repositories.WALLET);
+            Wallet wallet = walletRepository.query(new WalletByOwnerLogin(login)).get(0);
+            int walletId = wallet.getId();
+
+            Repository<WalletArtifacts> walletArtifactsRepository = (WalletArtifactsRepository)REPOSITORY_POOL
+                    .getRepository(Repositories.WALLET_ARTIFACTS);
+            WalletArtifacts walletArtifacts = walletArtifactsRepository
+                    .query(new WalletArtifactsById(walletId, artifactId)).get(0);
+
+            walletArtifacts.incrementQuantity();
+            walletArtifactsRepository.update(walletArtifacts);
 
         } catch (PersistenceLayerException e) {
 
