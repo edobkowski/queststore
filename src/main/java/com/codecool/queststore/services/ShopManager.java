d7ac900428ce315fcc0d1cf0dd6dcb79bbdc4bf0
Eryk <edobkowski@gmail.com>
2018-06-21 21:24:13 +0200

    Implement buyArtifact() method

--- src/main/java/com/codecool/queststore/services/ShopManager.java
+++ src/main/java/com/codecool/queststore/services/ShopManager.java
@@ -1,11 +1,15 @@
 package com.codecool.queststore.services;
 
+import com.codecool.queststore.criteria.ArtifactById;
 import com.codecool.queststore.criteria.SqlCriteria;
 import com.codecool.queststore.criteria.WalletArtifactsById;
 import com.codecool.queststore.criteria.WalletByOwnerLogin;
+import com.codecool.queststore.entities.Artifact;
 import com.codecool.queststore.entities.Wallet;
 import com.codecool.queststore.entities.WalletArtifacts;
 import com.codecool.queststore.repositories.*;
+
+import java.util.List;
 
 public class ShopManager {
     private static final RepositoryPool REPOSITORY_POOL;
@@ -20,15 +24,35 @@ public class ShopManager {
             Repository<Wallet> walletRepository = (WalletRepository)REPOSITORY_POOL
                     .getRepository(Repositories.WALLET);
             Wallet wallet = walletRepository.query(new WalletByOwnerLogin(login)).get(0);
+            int walletBalance = wallet.getBalance();
             int walletId = wallet.getId();
+
+            Repository<Artifact> artifactRepository = (ArtifactRepository)REPOSITORY_POOL
+                    .getRepository(Repositories.ARTIFACT);
+            Artifact artifact = artifactRepository.query(new ArtifactById(artifactId)).get(0);
+            int artifactPrice = artifact.getPrice();
+
+            if(walletBalance < artifactPrice) {
+                throw new PersistenceLayerException("No enough coolcoins!");
+            }
 
             Repository<WalletArtifacts> walletArtifactsRepository = (WalletArtifactsRepository)REPOSITORY_POOL
                     .getRepository(Repositories.WALLET_ARTIFACTS);
-            WalletArtifacts walletArtifacts = walletArtifactsRepository
-                    .query(new WalletArtifactsById(walletId, artifactId)).get(0);
+            List<WalletArtifacts> walletArtifactsList = walletArtifactsRepository
+                    .query(new WalletArtifactsById(walletId, artifactId));
+            WalletArtifacts walletArtifacts = new WalletArtifacts(walletId, artifactId);
+
+            if(walletArtifactsList.size() > 0) {
+                walletArtifacts = walletArtifactsList.get(0);
+            }
 
             walletArtifacts.incrementQuantity();
-            walletArtifactsRepository.update(walletArtifacts);
+
+            if(walletArtifacts.getQuantity() == 1) {
+                walletArtifactsRepository.add(walletArtifacts);
+            } else {
+                walletArtifactsRepository.update(walletArtifacts);
+            }
 
         } catch (PersistenceLayerException e) {
 
