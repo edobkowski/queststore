66a23c6b05e47ef790ddc267b5c849d77f9a6f03
Eryk <edobkowski@gmail.com>
2018-06-21 19:28:56 +0200

    Add criteria for getting wallet by ownerLogin

---
+++ src/main/java/com/codecool/queststore/criteria/WalletByOwnerLogin.java
@@ -0,0 +1,33 @@
+package com.codecool.queststore.criteria;
+
+import com.codecool.queststore.ConnectionProvider;
+import com.codecool.queststore.repositories.PersistenceLayerException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class WalletByOwnerLogin  implements SqlCriteria {
+    private final String QUERY = "SELECT * FROM wallets WHERE owner_login=?";
+    private final String login;
+
+    public WalletByOwnerLogin(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public PreparedStatement toPreparedStatement() throws PersistenceLayerException {
+        try {
+            Connection connection = ConnectionProvider.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(QUERY);
+
+            preparedStatement.setString(1, this.login);
+
+            return preparedStatement;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new PersistenceLayerException("Can't perform this query due to " +
+                    "exception occurance when creating PreparedStatement");
+        }
+    }
+}
