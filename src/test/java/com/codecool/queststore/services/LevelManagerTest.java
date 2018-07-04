bb4c3ed812d725fcb2fb75cf17c851bee40a6baa
WojTechM <wojciech.m1997@gmail.com>
2018-07-04 13:51:12 +0200

    Clean LevelManagerTest class

--- src/test/java/com/codecool/queststore/services/LevelManagerTest.java
+++ src/test/java/com/codecool/queststore/services/LevelManagerTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.mockito.Mockito.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 class LevelManagerTest {
 
